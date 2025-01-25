2b6714cc80f7d6e725092aab312936da95661865
Ryze <rrll20220@gmail.com>
2025-01-25 20:29:16 +0100

    modal productos

---
+++ assets/js/scipt.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    //$('body').css('background', 'red');
+    $.each($('.card'), function (indexInArray, valueOfElement) { 
+        $(this).on('click', function () {
+            let title = $(this).find('.card-title').text();
+            let text = $(this).find('.card-text').text();
+            let img = $(this).find('img');
+            let src = img.attr('src');
+
+            $('#productModalLabel').text(title);
+            $('#textModalLabel').text(text);
+            $('#modalImage').attr('src', src);
+        });
+    });
+});
