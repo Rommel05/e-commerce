43bdfa1d87923e549dae0a840ad185ed3335c947
Rommel <rrommel5@gmail.com>
2025-01-27 10:17:19 +0100

    carrito

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,25 @@ $(document).ready(function () {
 
             $('#productModalLabel').text(title);
             $('#priceModalLabel').text(text);
+            $('#priceModalLabel').css('font-weight', 'bold');
             $('#modalImage').attr('src', src);
         });
+    });
+
+    $('#addCart').on('click', function () {
+        //$('#priceModalLabel').html('<p>hola</p>');
+        if ($('#empty-text').length != 0) {
+            $('#empty-text').remove();
+        } 
+
+        let title = $('#productModalLabel').text();
+        let price = $('#priceModalLabel').text();
+        let image = $('#modalImage').attr('src');
+        let quantity = $('#number').val();
+
+
+        $('.offcanvas-body').append("<div class='cart-product'> <img src='"+ image + "'> <p>" + title + "</p> <p>" + price + "</p> <input type='text' value='"+ quantity +"' readonly> <button class='btn btn-outline-danger'><i class='bi bi-trash-fill'></i></button></div>");
+
+
     });
 });
