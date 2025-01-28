ca0375853205802c4dac67f58711a6db3a93e5a8
Rommel <rrommel5@gmail.com>
2025-01-28 10:27:44 +0100

    carrito funcional

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,25 @@
 $(document).ready(function () {
     //$('body').css('background', 'red');
-    $.each($('.card'), function (indexInArray, valueOfElement) { 
-        $(this).on('click', function () {
-            let title = $(this).find('.card-title').text();
-            let text = $(this).find('.card-text').text();
-            let img = $(this).find('img');
-            let src = img.attr('src');
 
-            $('#productModalLabel').text(title);
-            $('#priceModalLabel').text(text);
-            $('#priceModalLabel').css('font-weight', 'bold');
-            $('#modalImage').attr('src', src);
-        });
+    /* Validación campo number */
+    $('#number').val(1);
+    $('#number').attr('min',1);
+
+    /* Añadir dinḿicamente la informaciín del producto a la ventana modal */
+
+    $(document).on('click', '.card', function () {
+        let title = $(this).find('.card-title').text();
+        let text = $(this).find('.card-text').text();
+        let img = $(this).find('img');
+        let src = img.attr('src');
+    
+        $('#productModalLabel').text(title);
+        $('#priceModalLabel').text(text);
+        $('#priceModalLabel').css('font-weight', 'bold');
+        $('#modalImage').attr('src', src);
     });
+
+    /* Añadir al carrito */
 
     $('#addCart').on('click', function () {
         //$('#priceModalLabel').html('<p>hola</p>');
@@ -25,9 +32,34 @@ $(document).ready(function () {
         let image = $('#modalImage').attr('src');
         let quantity = $('#number').val();
 
+        let cardCart = `
+        <div class="cart-card">
+            <img src="${image}" alt="${title}">
+            <div class="info">
+                <h5>${title}</h5>
+                <p>Quantity: ${quantity}</p>
+                <p>Price: ${price}</p>
+            </div>
+            <div class="actions">
+                <button class="btn btn-outline-danger btn-sm remove-item">
+                    <i class="bi bi-trash-fill"></i> Eliminar
+                </button>
+            </div>
+        </div>`;
+        $('.offcanvas-body').append(cardCart);
+    });
 
-        $('.offcanvas-body').append("<div class='cart-product'> <img src='"+ image + "'> <p>" + title + "</p> <p>" + price + "</p> <input type='text' value='"+ quantity +"' readonly> <button class='btn btn-outline-danger'><i class='bi bi-trash-fill'></i></button></div>");
+    /* Borrar del carrito */
+    /*$.each($('.cart-card'), function (indexInArray, valueOfElement) { 
+         $('.remove-item').on('click', function () {
+            valueOfElement.remove();
+         });
+    });*/
 
-
+    $(document).on('click', '.remove-item', function () {
+        $(this).closest('.cart-card').remove(); // Elimina la tarjeta completa
+        if($('.cart-card').length == 0) {
+            $('.offcanvas-body').append(`<p id="empty-text">Your cart is empty</p>`);
+        }
     });
 });
